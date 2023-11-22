17c41c6b46c6f239ad7aca365e01cf13b5152ca4
Google <cemcimyildiz@gmail.com>
2023-11-21 23:29:59 -0800

    oops fixed that

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { FormRow, FormSection, FormSwitch } from "enmity/components";
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
 import { Messages, React, Toasts } from "enmity/metro/common";
 import { create } from "enmity/patcher";
-import SettingsPage from "../../common/components/_pluginSettings/settingsPage";
-import { Icons } from "../../common/components/_pluginSettings/utils";
+import SettingsPage from "../common/components/_pluginSettings/settingsPage";
+import { Icons } from "../common/components/_pluginSettings/utils";
 import manifest from "../manifest.json";
 
 interface SettingsProps {
