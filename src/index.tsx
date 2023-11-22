00f768420b3558e14b5bea903caf6269d10233d0
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-11-21 23:16:30 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,43 @@
-import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React } from 'enmity/metro/common';
-import { getByProps } from 'enmity/metro';
-import { create } from 'enmity/patcher';
-import manifest from '../manifest.json';
+import { get, set, SettingsStore } from "enmity/api/settings";
+import { FormRow, FormSection, FormSwitch } from "enmity/components";
+import { Plugin, registerPlugin } from "enmity/managers/plugins";
+import { Messages, React, Toasts } from "enmity/metro/common";
+import { create } from "enmity/patcher";
+import SettingsPage from "../../common/components/_pluginSettings/settingsPage";
+import { Icons } from "../../common/components/_pluginSettings/utils";
+import manifest from "../manifest.json";
 
-import Settings from './components/Settings';
+interface SettingsProps {
+    settings: SettingsStore;
+}
 
-const Typing = getByProps('startTyping');
-const Patcher = create('silent-typing');
+const Patcher = create(manifest.name);
 
-const SilentTyping: Plugin = {
-   ...manifest,
-
-   onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
-   },
-
-   onStop() {
-      Patcher.unpatchAll();
-   },
-
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
-   }
+const BetterInstagramEmbeds: Plugin = {
+    ...manifest,
+    onStart() {
+        try {
+            Patcher.before(Messages, "sendMessage", (_self, args, _orig) => {
+                const content = args[1]["content"];
+                const instagramLinks = content.match(/http(s)?:\/\/(www\.)?instagram\.com\/\w{4,15}\/p\/\w+/gim);
+                if (!instagramLinks) return;
+                args[1]["content"] = content.replace(/http(s)?:\/\/(www\.)?instagram\.com/gim, `https://www.ddinstagram.com`);
+            });
+        } catch (err) {
+            console.log("[ BetterInstagramEmbeds Error ]", err);
+        }
+    },
+    onStop() {
+        Patcher.unpatchAll();
+    },
+    patches: [],
+    getSettingsPanel({ settings }: SettingsProps) {
+        return <SettingsPage manifest={manifest} settings={settings} hasToasts={false} commands={null}>
+            <FormSection title="Plugin Settings">
+                {/* no settings are really necessary */}
+            </FormSection>
+        </SettingsPage>
+    },
 };
 
-registerPlugin(SilentTyping);
+registerPlugin(BetterInstagramEmbeds);
